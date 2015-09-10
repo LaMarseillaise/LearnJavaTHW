71c424da3c2c488c0b792fe1a6cfee723bfaf0c5
Andur Carr <carr.andur@gmail.com>
2015-09-09 20:41:06 -0700

    Complete through Exercise 13

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "I sense a soul in search of answers." );
+    System.out.println( "You seek the secret of time travel?" );
+  }
+}
---
+++ LetterToYourself.java
@@ -0,0 +1,15 @@
+public class LetterToYourself {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------------------------------+" );
+    System.out.println( "|                                          ####  |" );
+    System.out.println( "|                                          ####  |" );
+    System.out.println( "|                                          ####  |" );
+    System.out.println( "|                                                |" );
+    System.out.println( "|                                                |" );
+    System.out.println( "|                        Andur Carr              |" );
+    System.out.println( "|                        1 Somewhere Rd.         |" );
+    System.out.println( "|                        San Francisco, CA       |" );
+    System.out.println( "|                                                |" );
+    System.out.println( "+------------------------------------------------+" );
+  }
+}
